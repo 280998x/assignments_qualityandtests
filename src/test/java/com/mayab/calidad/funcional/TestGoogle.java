804e10b8a55ce81f9ba593ca8168a65e4593c4bc
280998x <39132320+280998x@users.noreply.github.com>
2020-11-29 18:12:32 -0500

    geckodriver

--- src/test/java/com/mayab/calidad/funcional/TestGoogle.java
+++ src/test/java/com/mayab/calidad/funcional/TestGoogle.java
@@ -21,7 +21,6 @@ public class TestGoogle {
 
 	  @Before
 	  public void setUp() throws Exception {
-		  System.setProperty("webdriver.gecko.driver", "geckodriver");
 		  driver = new FirefoxDriver();
 		  baseUrl = "https://www.google.com/";
 		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
