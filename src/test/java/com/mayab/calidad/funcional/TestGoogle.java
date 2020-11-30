6d8e15d16aabfd18909bd75f0089620a7ae54d18
280998x <39132320+280998x@users.noreply.github.com>
2020-11-30 09:23:16 -0500

    Update TestGoogle.java

--- src/test/java/com/mayab/calidad/funcional/TestGoogle.java
+++ src/test/java/com/mayab/calidad/funcional/TestGoogle.java
@@ -31,10 +31,10 @@ public class TestGoogle {
 	  public void testCase1() throws Exception {
 	    driver.get("https://www.google.com/");
 	    driver.findElement(By.name("q")).clear();
-	    driver.findElement(By.name("q")).sendKeys("usa gov covid");
+	    driver.findElement(By.name("q")).sendKeys("disneyworld.disney.go.com");
 	    driver.findElement(By.id("tsf")).submit();
 	    driver.findElement(By.xpath("/html/body/div[7]/div[2]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div[1]/a/h3/span")).click();
-	    assertEquals("Government Response to Coronavirus, COVID-19  | USAGov", driver.getTitle());
+	    assertEquals("Walt Disney World Resort in Orlando, Florida", driver.getTitle());
 	  }
 
 	  @After
